7cb748569b4c4f3a5b0a88c96f0243670b3b14e6
Adrien Dorsaz <adrien@adorsaz.ch>
2013-09-17 11:49:49 +0200

    Add createEpub function inspired by PHPePub 2.50 example.

--- epub.plugin.disabled.php
+++ epub.plugin.disabled.php
@@ -61,28 +61,75 @@ function epub_plugin_action($_,$myUser){
         exit('Vous devez vous connecter pour cette action.');
     }
     else{
-        $requete = 'SELECT id,title,guid,content,description,link,pubdate,unread,favorite
+        $requete = 'SELECT title,creator,content,pubdate
                     FROM '.MYSQL_PREFIX.'event
                     WHERE ';
-        if($_['action']=='epub_unread'){
+
+        $epubfile_title = "Title to be defined";
+
+        if(strpos($_['action'],'epub_unread')!==false){
             $requete.='unread=1';
-        }elseif($_['action']=='epub_favorites'){
+            $epubfile_title = 'Articles Leed Non lu - le '.date('d/m/y').' à '.date('H:i');
+        }elseif(strpos($_['action'],'epub_favorites')!==false){
             $requete.='favorite=1';
+            $epubfile_title = 'Articles Leed Favoris - le '.date('d/m/y').' à '.date('H:i');
         }
-        $query = mysql_query($requete);
-        // TODO Create EPUB here
+        
+        $query = mysql_query($requete); // TODO PHP 5.5.0, remove this function to use mysqli_query or PDO
+
         if($query){
-            while($data=mysql_fetch_array($query)){
-                // TODO Add chapters here
-                echo $data['id'];
-            }
-            // TODO Finalize EPUB and send it
+            create_epub($epubfile_title, $query, strpos($_['action'],'full')!==false);
         }else{
             echo mysql_error();
         }
     }
 }
 
+/* Utilise le contenu des articles pour créer un livre epub */
+function create_epub($title, $qry_articles, $external_content){
+    $nbArticles = mysql_num_rows($qry_articles); // TODO PHP 5.5.0, remove this function to use mysqli_stmt_num_rows or PDO
+    
+    if($nbArticles>0){
+        // Epub initialisation
+        $book = new EPub();
+        $epubbook_start = constant("EPUBBOOK_HEAD_START").$title.constant("EPUBBOOK_HEAD_END");
+        
+        $book->setTitle($title);
+        $book->setIdentifier("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."/".$title, EPub::IDENTIFIER_URI);
+        $book->setLanguage("fr"); // TODO L10N
+        $book->setDescription("Auto generated epub file with artilces from Leed rss reader.");
+        //$book->addCSSFile("styles.css", "css1", $cssData); //TODO add Leed css ?
+        //$book->setCoverImage("Cover.jpg", file_get_contents("demo/cover-image.jpg"), "image/jpeg");
+        
+        $cover = $epubbook_start. "<h1>".$title."</h1>\n<h2>By your Leed rss reader</h2>\n" . constant("EPUBBOOK_END");
+        $book->addChapter("Notices", "Cover.html", $cover);
+        
+        // Epub chapters (articles) creation
+        $chapNb = 1;
+        while($data=mysql_fetch_array($qry_articles)){
+            $html_content = $epubbook_start
+                . '<h2 class="articleTitle">'.$data['title'].'</h2>'
+                . '<h3 class="articleDetails"> par '.$data['creator'].' le '.date("d/m/Y à H:i:s",$data['pubdate']).'</h3>'
+                . $data['content'].constant("EPUBBOOK_END");
+            
+            if($external_content){
+                $book->addChapter($data['title'], "Chapitre_".$chapNb.".html", $html_content, true, EPub::EXTERNAL_REF_ADD);
+            }else{
+                $book->addChapter($data['title'], "Chapitre_".$chapNb.".html", $html_content, true, EPub::EXTERNAL_REF_IGNORE);
+            }
+            
+            $chapNb++;
+        }
+        
+        // Epub finalization
+        $book->buildTOC();
+        $book->finalize();
+        $zipData = $book->sendBook(preg_replace("( )", '_', preg_replace("([^\w\s\d\,;\[\]\(\]])", '', $title)));
+        exit;
+    }else{
+        echo "Aucun articles à mettre dans le fichier epub.";
+    }
+}
 
 // Ajout de la fonction epub_plugin_displayEvents au Hook situé après le menu des flux
 Plugin::addHook("menu_post_folder_menu", "epub_plugin_menu");
